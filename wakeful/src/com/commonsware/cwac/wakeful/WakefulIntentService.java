605c38a97e0b05d4c3420c5c31f65b20564f12df
Sean Katz <sean.katz@gmail.com>
2015-02-28 19:12:35 +0200

    Fixing new API 21 issue - "java.lang.IllegalArgumentException: Service Intent must be explicit", by turning all implicit intents to explicit intents automatically (Read here: http://blog.android-develop.com/2014/10/android-l-api-21-javalangillegalargumen.html)

--- wakeful/src/com/commonsware/cwac/wakeful/WakefulIntentService.java
+++ wakeful/src/com/commonsware/cwac/wakeful/WakefulIntentService.java
@@ -17,11 +17,16 @@ package com.commonsware.cwac.wakeful;
 import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.PowerManager;
 import android.util.Log;
+
+import java.util.List;
 
 abstract public class WakefulIntentService extends IntentService {
   abstract protected void doWakefulWork(Intent intent);
@@ -45,8 +50,49 @@ abstract public class WakefulIntentService extends IntentService {
 
   public static void sendWakefulWork(Context ctxt, Intent i) {
     getLock(ctxt.getApplicationContext()).acquire();
+    Intent explicitIntent = createExplicitFromImplicitIntent(ctxt, i);
     ctxt.startService(i);
   }
+
+  /**
+   * Android L (lollipop, API 21) introduced a new problem when trying to invoke implicit intent,
+   * "java.lang.IllegalArgumentException: Service Intent must be explicit"
+   * <p/>
+   * If you are using an implicit intent, and know only 1 target would answer this intent,
+   * This method will help you turn the implicit intent into the explicit form.
+   * <p/>
+   * Inspired from SO answer: http://stackoverflow.com/a/26318757/1446466
+   *
+   * @param implicitIntent - The original implicit intent
+   * @return Explicit Intent created from the implicit original intent
+   */
+  public static Intent createExplicitFromImplicitIntent(Context context, Intent implicitIntent) {
+      // Retrieve all services that can match the given intent
+      PackageManager pm = context.getPackageManager();
+      List<ResolveInfo> resolveInfo = pm.queryIntentServices(implicitIntent, 0);
+
+      // Make sure only one match was found
+      if (resolveInfo == null || resolveInfo.size() != 1) {
+          Log.e("WakefulIntentService", "Couldn't find a single match for intent " + implicitIntent +
+                  ". Make sure you configured the intent correctly.");
+          return null;
+      }
+
+      // Get component info and create ComponentName
+      ResolveInfo serviceInfo = resolveInfo.get(0);
+      String packageName = serviceInfo.serviceInfo.packageName;
+      String className = serviceInfo.serviceInfo.name;
+      ComponentName component = new ComponentName(packageName, className);
+
+      // Create a new intent. Use the old one for extras and such reuse
+      Intent explicitIntent = new Intent(implicitIntent);
+
+      // Set the component to be explicit
+      explicitIntent.setComponent(component);
+
+      return explicitIntent;
+  }
+
 
   public static void sendWakefulWork(Context ctxt, Class<?> clsService) {
     sendWakefulWork(ctxt, new Intent(ctxt, clsService));
